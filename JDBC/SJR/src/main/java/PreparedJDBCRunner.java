be4167d9595a717787b49f706e094ea5e8c44dcf
Arslan Akavov <arsakavov@gmail.com>
2017-08-17 11:29:12 +0300

    add PreparedJDBCRunner

---
+++ JDBC/SJR/src/main/java/PreparedJDBCRunner.java
@@ -0,0 +1,34 @@
+import javafx.scene.chart.PieChart;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+/**
+ * Created by Ars on 17.08.2017.
+ */
+public class PreparedJDBCRunner {
+    public static void main(String[] args) {
+
+        ArrayList<Abonent> list=new ArrayList<Abonent>(){
+            {
+                add(new Abonent(87, 1658468, "Кожух Дмитрий"));
+                add(new Abonent(51, 8866711, "Буйкевич Александр"));
+            }
+        };
+        DataBaseHelper helper =null;
+        PreparedStatement statement=null;
+
+        try {
+            helper=new DataBaseHelper();
+            statement=helper.getPreparedStatement();
+            for (Abonent abonent:list){
+                helper.insertAbonent(statement,abonent);
+            }
+        } catch (SQLException e){
+            e.printStackTrace();
+        } finally {
+            helper.closeStatement(statement);
+        }
+    }
+}
